0d982641f6d794b34112a716d503e94744312d21
Le Minh Huong <huonglm@qsoft.com.vn>
2016-11-14 16:53:22 +0700

    init project

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://demo.vn/admin/');
+define('HTTP_CATALOG', 'http://demo.vn/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://demo.vn/admin/');
+define('HTTPS_CATALOG', 'http://demo.vn/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Programs/htdocs/shop368/admin/');
+define('DIR_SYSTEM', 'D:/Programs/htdocs/shop368/system/');
+define('DIR_IMAGE', 'D:/Programs/htdocs/shop368/image/');
+define('DIR_LANGUAGE', 'D:/Programs/htdocs/shop368/admin/language/');
+define('DIR_TEMPLATE', 'D:/Programs/htdocs/shop368/admin/view/template/');
+define('DIR_CONFIG', 'D:/Programs/htdocs/shop368/system/config/');
+define('DIR_CACHE', 'D:/Programs/htdocs/shop368/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Programs/htdocs/shop368/system/storage/download/');
+define('DIR_LOGS', 'D:/Programs/htdocs/shop368/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Programs/htdocs/shop368/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Programs/htdocs/shop368/system/storage/upload/');
+define('DIR_CATALOG', 'D:/Programs/htdocs/shop368/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shop368');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'bshop_');
