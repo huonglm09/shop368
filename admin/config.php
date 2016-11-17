d27ddbe04e02ee4fa499276ffa8418b81e7f7915
Le Minh Huong <huonglm@qsoft.com.vn>
2016-11-17 13:59:21 +0700

    update

--- admin/config.php
+++ admin/config.php
@@ -1,25 +1,25 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://bshop.vn/admin/');
-define('HTTP_CATALOG', 'http://bshop.vn/');
+define('HTTP_SERVER', 'http://demo.vn/admin/');
+define('HTTP_CATALOG', 'http://demo.vn/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://bshop.vn/admin/');
-define('HTTPS_CATALOG', 'http://bshop.vn/');
+define('HTTPS_SERVER', 'http://demo.vn/admin/');
+define('HTTPS_CATALOG', 'http://demo.vn/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/xampp/htdocs/shop368/admin/');
-define('DIR_SYSTEM', 'C:/xampp/htdocs/shop368/system/');
-define('DIR_IMAGE', 'C:/xampp/htdocs/shop368/image/');
-define('DIR_LANGUAGE', 'C:/xampp/htdocs/shop368/admin/language/');
-define('DIR_TEMPLATE', 'C:/xampp/htdocs/shop368/admin/view/template/');
-define('DIR_CONFIG', 'C:/xampp/htdocs/shop368/system/config/');
-define('DIR_CACHE', 'C:/xampp/htdocs/shop368/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/xampp/htdocs/shop368/system/storage/download/');
-define('DIR_LOGS', 'C:/xampp/htdocs/shop368/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/xampp/htdocs/shop368/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/xampp/htdocs/shop368/system/storage/upload/');
-define('DIR_CATALOG', 'C:/xampp/htdocs/shop368/catalog/');
+define('DIR_APPLICATION', 'D:/Programs/htdocs/shop368/admin/');
+define('DIR_SYSTEM', 'D:/Programs/htdocs/shop368/system/');
+define('DIR_IMAGE', 'D:/Programs/htdocs/shop368/image/');
+define('DIR_LANGUAGE', 'D:/Programs/htdocs/shop368/admin/language/');
+define('DIR_TEMPLATE', 'D:/Programs/htdocs/shop368/admin/view/template/');
+define('DIR_CONFIG', 'D:/Programs/htdocs/shop368/system/config/');
+define('DIR_CACHE', 'D:/Programs/htdocs/shop368/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Programs/htdocs/shop368/system/storage/download/');
+define('DIR_LOGS', 'D:/Programs/htdocs/shop368/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Programs/htdocs/shop368/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Programs/htdocs/shop368/system/storage/upload/');
+define('DIR_CATALOG', 'D:/Programs/htdocs/shop368/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
