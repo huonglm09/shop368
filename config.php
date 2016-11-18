7682836c3e4e15537c6427e6ef4c32fe920b45c9
Le Minh Huong <huonglm@qsoft.com.vn>
2016-11-18 12:00:46 +0700

    update

--- config.php
+++ config.php
@@ -1,22 +1,22 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://bshop.vn/');
+define('HTTP_SERVER', 'http://demo.vn/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://bshop.vn/');
+define('HTTPS_SERVER', 'http://demo.vn/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/xampp/htdocs/shop368/catalog/');
-define('DIR_SYSTEM', 'C:/xampp/htdocs/shop368/system/');
-define('DIR_IMAGE', 'C:/xampp/htdocs/shop368/image/');
-define('DIR_LANGUAGE', 'C:/xampp/htdocs/shop368/catalog/language/');
-define('DIR_TEMPLATE', 'C:/xampp/htdocs/shop368/catalog/view/theme/');
-define('DIR_CONFIG', 'C:/xampp/htdocs/shop368/system/config/');
-define('DIR_CACHE', 'C:/xampp/htdocs/shop368/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/xampp/htdocs/shop368/system/storage/download/');
-define('DIR_LOGS', 'C:/xampp/htdocs/shop368/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/xampp/htdocs/shop368/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/xampp/htdocs/shop368/system/storage/upload/');
+define('DIR_APPLICATION', 'D:/Programs/htdocs/shop368/catalog/');
+define('DIR_SYSTEM', 'D:/Programs/htdocs/shop368/system/');
+define('DIR_IMAGE', 'D:/Programs/htdocs/shop368/image/');
+define('DIR_LANGUAGE', 'D:/Programs/htdocs/shop368/catalog/language/');
+define('DIR_TEMPLATE', 'D:/Programs/htdocs/shop368/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/Programs/htdocs/shop368/system/config/');
+define('DIR_CACHE', 'D:/Programs/htdocs/shop368/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Programs/htdocs/shop368/system/storage/download/');
+define('DIR_LOGS', 'D:/Programs/htdocs/shop368/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Programs/htdocs/shop368/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Programs/htdocs/shop368/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
@@ -25,4 +25,4 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'shop368');
 define('DB_PORT', '3306');
-define('DB_PREFIX', 'bshop_');
+define('DB_PREFIX', 'demo_');
